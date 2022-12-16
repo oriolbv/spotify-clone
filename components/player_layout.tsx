ce5f3e3dbc0f5e07a54cd26e0b910cd53f40e69a
Oriol Burgaya <oriolbv@gmail.com>
2022-12-16 14:57:47 +0100

    Sidebar added

--- components/player_layout.tsx
+++ components/player_layout.tsx
@@ -1,13 +1,30 @@
-import {Box} from '@chakra-ui/layout';
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay
+} from "@chakra-ui/layout";
+import {MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite} from "react-icons/md";
+import Sidebar from "./sidebar";
 
-const PlayerLayout = ({children}) => {
-    return (
-        <Box>
-            Layout
-            {children}
-        </Box>
-    )
-
-}
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" width="250px" left="0">
+        <Sidebar />
+      </Box>
+      <Box marginLeft="250px" marginBottom="100px">
+        {children}
+      </Box>
+      <Box position="absolute" left="0" bottom="0">
+        player
+      </Box>
+    </Box>
+  );
+};
 
 export default PlayerLayout;
---
+++ components/sidebar.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/layout";
+
+const Sidebar = ({ children }) => {
+  return <Box>hello from sidebar</Box>;
+};
+
+export default Sidebar;
